6bb8d55951c2985d0ebcdfce7e21966d3758d1b5
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-12 16:16:15 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@all = []
+
+    def initialize(params)
+        self.name = params[:name]
+        self.weight = params[:weight]
+        self.height = params[:height]
+        self.class.all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@all = []
+
+    def initialize(params)
+        self.name = params[:name]
+        self.type = params[:type]
+        self.booty = params[:booty]
+        self.class.all.push(self)
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
 end
